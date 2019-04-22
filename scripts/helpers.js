1bc4ab1bff09a9946b470218ece7dde4471d59fa
yinguangyao <1451830100@qq.com>
2019-04-22 22:15:54 +0800

    feat(rewrite): rewrite all css

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,12 +1,33 @@
 // 获取文章摘要
-hexo.extend.helper.register('excerpt', function(content, len) {
+hexo.extend.helper.register('excerpt', function (content, len) {
   if (!content) {
     return '...';
   }
   // 去除html标签
-  content = content.replace(/<[^>]+>/g,"");
+  content = content.replace(/<[^>]+>/g, "");
   if (content.length > len) {
     return content.substr(0, len) + '...';
   }
   return content.substr(0, len);
 });
+var autoCompleTime = function(time) {
+  if (time >= 10) {
+    return time;
+  }
+  return '0' + time
+}
+hexo.extend.helper.register('GMT2Str', function (time) {
+  var date, str;
+  if (!time) {
+    date = new Date()
+  } else {
+    date = new Date(time)
+  }
+  str = date.getFullYear() + '-' +
+    autoCompleTime(date.getMonth() + 1) + '-' +
+    autoCompleTime(date.getDate()) + ' ' +
+    autoCompleTime(date.getHours()) + ':' +
+    autoCompleTime(date.getMinutes()) + ':' +
+    autoCompleTime(date.getSeconds())
+  return str
+})
--- source/js/events.js
+++
@@ -1,6 +0,0 @@
-var gnav = document.getElementById('gnav')
-gnav.addEventListener('click', function(e) {
-    if (e.target.nodeName === 'li') {
-
-    }
-})
